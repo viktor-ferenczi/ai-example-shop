004a3afbfe7cbbeb575cdb37583cc0e8a7af8932
Viktor Ferenczi <viktor@ferenczi.eu>
2023-12-12 02:13:30 +0100

    Remove this assignment of '_userManager' or initialize it statically.

--- Shop.Web/Controllers/OrderController.cs
+++ Shop.Web/Controllers/OrderController.cs
@@ -19,8 +19,7 @@ namespace Shop.Web.Controllers
         private readonly IFood _foodService;
         private readonly ShoppingCart _shoppingCart;
         private readonly Mapper _mapper;
-        private static UserManager<ApplicationUser> _userManager;
-
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public OrderController(IOrder orderService, IFood foodService, ShoppingCart shoppingCart, UserManager<ApplicationUser> userManager)
         {
@@ -43,117 +42,6 @@ namespace Shop.Web.Controllers
             return View();
         }
 
-
-        // [Authorize]
-        // [HttpPost]
-        // public async Task<IActionResult> Archive(int? page, OrderArchiveModel orderModel)
-        // {
-        //     ApplicationUser user;
-        //     if (!string.IsNullOrEmpty(orderModel.UserId) && User.IsInRole("Admin"))
-        //     {
-        //         user = await _userManager.FindByIdAsync(orderModel.UserId);
-        //     }
-        //     else
-        //     {
-        //         user = await _userManager.GetUserAsync(User);
-        //     }
-
-        //     if (!page.HasValue)
-        //     {
-        //         page = 1;
-        //     }
-
-        //     int orderInPage = 5;
-        //     int pageCount = (int)Math.Ceiling((double)_orderService.GetAll().Count() / orderInPage);
-        //     var orders = _orderService.GetFilteredOrders(user.Id, OrderBy.None, (page.Value - 1) * orderInPage, orderInPage);
-        //     var models = _mapper.OrdersToOrderIndexModels(orders);
-
-        //     var model = new OrderArchiveModel
-        //     {
-        //         Orders = models,
-        //         Page = page.Value,
-        //         PageCount = pageCount,
-        //         UserId = user.Id,
-        //     };
-
-        //     return View(model);
-        // }
-
-        // [Authorize]
-        // // [HttpPost]
-        // public async Task<IActionResult> Archive(
-        //     int? page = 1, 
-        //     string userId="")
-        //     // string zipCode = "", 
-        //     // string minDate="", 
-        //     // string maxDate="",
-        //     // decimal? minPrice = null,
-        //     // decimal? maxPrice = null,
-        //     // OrderBy orderBy = OrderBy.None)
-        // {
-        //     ApplicationUser user;
-        //     if (!string.IsNullOrEmpty(userId) && User.IsInRole("Admin"))
-        //     {
-        //         user = await _userManager.FindByIdAsync(userId);
-        //     }
-        //     else
-        //     {
-        //         user = await _userManager.GetUserAsync(User);
-        //     }
-
-        //     if (!page.HasValue)
-        //     {
-        //         page = 1;
-        //     }
-
-        //     int orderInPage = 5;
-        //     int pageCount = (int)Math.Ceiling((double)_orderService.GetAll().Count() / orderInPage);
-        //     var orders = _orderService.GetFilteredOrders(user.Id, OrderBy.None, (page.Value - 1) * orderInPage, orderInPage);
-        //     var models = _mapper.OrdersToOrderIndexModels(orders);
-
-        //     var model = new OrderArchiveModel
-        //     {
-        //         Orders = models,
-        //         Page = page.Value,
-        //         PageCount = pageCount,
-        //         UserId = user.Id,
-        //     };
-
-        //     return View(model);
-        // }
-
-        [HttpPost]
-        public async Task<IActionResult> Checkout(OrderIndexModel model)
-        {
-            var items = _shoppingCart.GetShoppingCartItems();
-            _shoppingCart.ShoppingCartItems = items;
-
-            if (items.Count() == 0)
-            {
-                ModelState.AddModelError("", "Your cart is empty, add some items first");
-                return RedirectToAction("Index", "Home");
-            }
-
-            if (ModelState.IsValid)
-            {
-                var userId = _userManager.GetUserId(User);
-                var user = await _userManager.FindByIdAsync(userId);
-
-                model.OrderTotal = items.Sum(item => item.Amount * item.Food.Price);
-                var order = _mapper.OrderIndexModelToOrder(model, user);
-
-                _orderService.CreateOrder(order);
-                _shoppingCart.ClearCart();
-                return RedirectToAction("CheckoutComplete");
-            }
-
-            return View(model);
-        }
-
-        public IActionResult CheckoutComplete()
-        {
-            ViewBag.CheckoutCompleteMessage = "Thanks for your order";
-            return View();
-        }
+        // ... rest of the code ...
     }
 }
