b03a9567ffa663a567f0036f7b0e376a7c6808c0
Viktor Ferenczi <viktor@ferenczi.eu>
2024-01-03 01:06:19 +0100

    Removed the batch file to analyze the project (integrated into ai-dev)

--- analyze.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-dotnet sonarscanner begin /k:"Shop" /d:sonar.token="%SONAR_TOKEN%" || exit /b 1
-dotnet build || exit /b 1
-dotnet sonarscanner end /d:sonar.token="%SONAR_TOKEN%" || exit /b 1
-echo Done
