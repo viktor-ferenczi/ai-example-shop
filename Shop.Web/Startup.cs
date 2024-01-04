c4888774077a59c0713e60223ecec0c6d2655269
Viktor Ferenczi <viktor@ferenczi.eu>
2024-01-04 19:20:21 +0100

    Formatted code

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -28,14 +28,14 @@ namespace Shop.Web
         }
 
         public IConfiguration Configuration { get; }
-        
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
-            
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
+
 
             services.AddIdentity<ApplicationUser, IdentityRole>(
                options =>
@@ -63,12 +63,12 @@ namespace Shop.Web
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sp => ShoppingCart.GetCart(sp));
-            
-			services.AddMvc(options => { options.EnableEndpointRouting = false;});
-			services.AddMemoryCache();
-			services.AddSession();
-			
-		}
+
+            services.AddMvc(options => { options.EnableEndpointRouting = false; });
+            services.AddMemoryCache();
+            services.AddSession();
+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
